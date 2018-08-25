d2b008fe4376956c27416b7bfc4a640343fad187
wbpandos <whiteboypandos@me.com>
2018-08-25 05:51:49 -0700

    pyramid blog commit

---
+++ pyramid_blogr/pyramid_blogr/forms.py
@@ -0,0 +1,20 @@
+from wtforms import Form, StringField, TextAreaField, validators
+from wtforms import IntegerField, PasswordField
+from wtforms.widgets import HiddenInput
+
+strip_filter = lambda x: x.strip() if x else None
+
+class BlogCreateForm(Form):
+    title = StringField('Title', [validators.Length(min=1, max=255)],
+                        filters=[strip_filter])
+    body = TextAreaField('Contents', [validators.Length(min=1)],
+                         filters=[strip_filter])
+
+class BlogUpdateForm(BlogCreateForm):
+    id = IntegerField(widget=HiddenInput())
+
+
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=1, max=255)],
+                           filters=[strip_filter])
+    password = PasswordField('Password', [validators.Length(min=3)])
